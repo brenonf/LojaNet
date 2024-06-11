797aabe1ef1dd42d78a640f9da37e78c3e981323
Breno Faria <brenonf@gmail.com>
2024-06-11 16:32:32 -0300

    Adição de interface de usuário para excluir,incluir e exibir detalhes de clientes

--- LojasNet.UI.Web/Controllers/ClienteController.cs
+++ LojasNet.UI.Web/Controllers/ClienteController.cs
@@ -2,11 +2,46 @@
 using Microsoft.AspNetCore.Mvc;
 using LojaNet.Models;
 using LojaNet.BLL;
+using Microsoft.AspNetCore.Http;
 
 namespace LojasNet.UI.Web.Controllers
 {
     public class ClienteController : Controller
     {
+        private ClienteBLL bll;
+
+        public ClienteController()
+        {
+            bll = new ClienteBLL();
+        }
+
+        public ActionResult Detalhes(string id)
+        {
+            var cliente =bll.ObterPorId(id);
+            return View(cliente);
+        }
+
+        public ActionResult Alterar(string id)
+        {
+            var cliente = bll.ObterPorId(id);
+            return View(cliente);
+        }
+
+        [HttpPost]
+        public ActionResult Alterar(Cliente cliente)
+        {
+            try
+            {
+                bll.Alterar(cliente);
+                return RedirectToAction("index");
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, ex.Message);
+                return View(cliente);
+            }
+        }
+
         public ActionResult Incluir()
         {
             var cliente = new Cliente();
@@ -18,7 +53,6 @@ namespace LojasNet.UI.Web.Controllers
         {
             try
             {
-                var bll = new ClienteBLL();
                 bll.Incluir(cliente);
                 return RedirectToAction("index");
             }
@@ -29,9 +63,30 @@ namespace LojasNet.UI.Web.Controllers
             }
         }
 
+        public ActionResult Excluir(string id)
+        {
+            var cliente = bll.ObterPorId(id);
+            return View(cliente);
+        }
+
+        [HttpPost]
+        public ActionResult ConfirmarExcluir(string id)
+        {
+            try
+            {
+                bll.Excluir(id);
+                return RedirectToAction("index");
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, ex.Message);
+                var cliente = bll.ObterPorId(id);
+                return View(cliente);
+            }
+        }
+
         public IActionResult Index()
         {
-            var bll = new ClienteBLL();
             var lista = bll.ObterTodos();
             return View(lista);
         }
