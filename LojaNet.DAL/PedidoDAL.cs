4923efba055b2b489967facb7e8d1d773793e524
Breno Faria <brenonf@gmail.com>
2024-07-03 18:02:35 -0300

    Cont.implementação de pedidos - testes

---
+++ LojaNet.DAL/PedidoDAL.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LojaNet.Models;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace LojaNet.DAL
+{
+    public class PedidoDAL : IPedidosDados
+    {
+        public void Alterar(Pedido pedido)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Excluir(int pedidoId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Incluir(Pedido pedido)
+        {
+            var cn = new SqlConnection(DbHelper.conexao);
+
+            var cmd1 = new SqlCommand("PedidoIncluir");
+            cmd1.Connection = cn;
+            cmd1.CommandType = CommandType.StoredProcedure;
+
+            var cmd2 = new SqlCommand("PedidoItemIncluir");
+            cmd2.Connection = cn;
+            cmd2.CommandType = CommandType.StoredProcedure;
+
+            cn.Open();
+            var tx = cn.BeginTransaction();
+
+            try
+            {
+                cmd1.Transaction = tx;
+                cmd2.Transaction = tx;
+
+                cmd1.Parameters.AddWithValue("@Data", pedido.Data);
+                cmd1.Parameters.AddWithValue("@ClienteId", pedido.Cliente.Id);
+                cmd1.Parameters.AddWithValue("@FormaPagamentoId", pedido.FormaPagamento);
+
+                pedido.Id = Convert.ToInt32(cmd1.ExecuteScalar());
+
+                int ordem = 1;
+                cmd2.Parameters.AddWithValue("@PedidoId", pedido.Id);
+                cmd2.Parameters.AddWithValue("@Ordem", 0);
+                cmd2.Parameters.AddWithValue("@Quantidade", 0);
+                cmd2.Parameters.AddWithValue("@Preco", Convert.ToDecimal(0));
+                cmd2.Parameters.AddWithValue("@ProdutoId", string.Empty);
+
+
+
+                foreach (var item in pedido.Itens)
+                {
+                    cmd2.Parameters["@ProdutoId"].Value = item.Produto.Id;
+                    cmd2.Parameters["@Ordem"].Value = ordem;
+                    cmd2.Parameters["@Quantidade"].Value = item.Quantidade;
+                    cmd2.Parameters["@Preco"].Value = item.Preco;
+                    cmd2.ExecuteNonQuery();
+                    ordem += 1;
+                }
+
+                tx.Commit();
+            }
+            catch(Exception ex) 
+            { 
+                tx.Rollback();
+                throw new Exception("Erro no servidor:" + ex.Message);
+            }
+            finally
+            { cn.Close(); }
+
+        }
+
+        public Pedido ObterPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Pedido> ObterTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LojaNet.Test/PedidoUnitTest.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using LojaNet.DAL;
+using Assert = NUnit.Framework.Assert;
+using System;
+using System.Net.Http;
+using LojaNet.Models;
+using System.Collections.Generic;
+
+namespace LojaNet.Test
+{
+    [TestFixture]
+    public class PedidoUnitTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Incluir()
+        {
+            var pedido = new Pedido();
+            pedido.Data = DateTime.Now;
+            pedido.Cliente = new Cliente() { Id = "03cd6093-fca8-41cf-bca8-0226553a74b6", Nome = "Breno N. Faria", Telefone = "15161561", Email = "515155" };
+            pedido.FormaPagamento = FormaPagamentoEnum.Pix;
+            pedido.Itens = new List<Pedido.Item>();
+
+            var item = new Pedido.Item();
+            item.Produto = new Produto() { Id = "36ec80fe-553d-4486-bbe0-0b40331b3c5e", Nome = "Estojo", Preco = 15.90m, Estoque = 22 };
+            item.Quantidade = 1;
+            item.Ordem = 3;
+            item.Preco = 15.90m;
+
+            pedido.Itens.Add(item);
+
+
+
+            item = new Pedido.Item();
+            item.Produto = new Produto() { Id = "94ee0698-4eef-471e-985e-0edd84fbb727", Nome = "Papel", Preco = 11.99m, Estoque = 65 };
+            item.Quantidade = 2;
+            item.Ordem = 3;
+            item.Preco = 11.99m;
+
+            pedido.Itens.Add(item);
+
+
+            var dal = new PedidoDAL();
+            dal.Incluir(pedido);
+
+        }
+    
+    }
+}
